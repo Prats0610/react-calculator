d6ad2eab352e15329027aa152251ae2db86e4e91
Prats0610 <97311902+Prats0610@users.noreply.github.com>
2022-01-14 14:57:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App(){
   const [result, setResult]= useState("");
   
   const calcButtons = [
-    '1','2','3','*','4','5','6','+','7','8','9','-'
+    '1','2','3','*','4','5','6','+','7','8','9','-','.','0'
   ]
 
   const operators= ["/", "+", "-", "*", "."];
