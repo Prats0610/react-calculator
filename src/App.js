e85ee74919c6ea47c1a06ab5ea0b3bc95afe40f2
Prats0610 <97311902+Prats0610@users.noreply.github.com>
2022-01-14 14:55:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -61,20 +61,7 @@ function App(){
      <button className='span-two' onClick={deleteLast}>DEL</button> 
 {
   calcButtons?.map(item =>  <button onClick = {()=>updateCalc(item)}>{item}</button>)}
-//      <button onClick = {()=>updateCalc("1")}>1</button>
-//      <button onClick = {()=>updateCalc("2")}>2</button>
-//      <button onClick = {()=>updateCalc("3")}>3</button>
-//      <button onClick = {()=>updateCalc("*")}>*</button>
-//      <button onClick = {()=>updateCalc("4")}>4</button>
-//      <button onClick = {()=>updateCalc("5")}>5</button>
-//      <button onClick = {()=>updateCalc("6")}>6</button>
-//      <button onClick = {()=>updateCalc("+")}>+</button>
-//      <button onClick = {()=>updateCalc("7")}>7</button>
-//      <button onClick = {()=>updateCalc("8")}>8</button>
-//      <button onClick = {()=>updateCalc("9")}>9</button>
-//      <button onClick = {()=>updateCalc("-")}>-</button>
-//      <button onClick = {()=>updateCalc(".")}>.</button>
-//      <button onClick = {()=>updateCalc("0")}>0</button>
+
      <button onClick={calculate}>=</button>
      <button onClick = {()=>updateCalc("/")}>/</button>
     </div>
